4b4bb04123aeddc604397d8416ab48d737f6a33d
Jon Erickson <jon@jnericks.com>
2016-05-17 09:06:23 -0700

    updates to base unit tester to include more helper verificationmodes and power mockito mocking methods, also added javadoc

--- src/main/java/com/jnericks/testlib/BaseUnitTester.java
+++ src/main/java/com/jnericks/testlib/BaseUnitTester.java
@@ -1,11 +1,51 @@
 package com.jnericks.testlib;
 
+import com.google.common.reflect.TypeToken;
+
 import org.mockito.verification.VerificationMode;
 
+import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.times;
+import static org.powermock.api.mockito.PowerMockito.mock;
 
 public abstract class BaseUnitTester
 {
-    public final VerificationMode NeverReceive = times(0);
-    public final VerificationMode ReceivedOnce = times(1);
+    /**
+     * Verifying exactly 0 invocations
+     */
+    protected static final VerificationMode NeverReceive = times(0);
+
+    /**
+     * Verifying exactly 1 invocations
+     */
+    protected static final VerificationMode ReceivedOnce = times(1);
+
+    /*
+     * Verifying at-least-once invocation
+     */
+    protected static final VerificationMode ReceiveAtLeastOnce = atLeastOnce();
+
+    /**
+     * Creates a mock object that supports mocking of final and native methods.
+     *
+     * @param <T>  the type of the mock object
+     * @param type the type of the mock object
+     * @return the mock object.
+     */
+    protected static <T> T an(Class<T> type)
+    {
+        return mock(type);
+    }
+
+    /**
+     * Creates a mock object that supports mocking of final and native methods.
+     *
+     * @param <T>       the type of the mock object
+     * @param typeToken the type token of the mock object
+     * @return the mock object.
+     */
+    public static <T> T an(TypeToken<T> typeToken)
+    {
+        return mock((Class<T>) typeToken.getRawType());
+    }
 }
